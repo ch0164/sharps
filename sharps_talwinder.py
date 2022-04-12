a1df67df076aa927d0f9b3f626c5df975097a918
Christian Hall <ch0164@uah.edu>
2022-04-12 04:39:33 -0500

    Parallel coordinates

--- sharps_talwinder.py
+++ sharps_talwinder.py
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 import numpy.typing as npt
 import pandas as pd
-from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import PolynomialFeatures, MinMaxScaler
 
 FLARE_PROPERTIES = [
     'ABSNJZH',
@@ -52,18 +52,45 @@ def parse_tai_string(tstr, datetime=True):
         return year, month, day, hour, minute
 
 
+def classify_flare(magnitude):
+    if "B" in magnitude:
+        return "B"
+    elif "C" in magnitude:
+        return "C"
+    elif "M" in magnitude:
+        return "M"
+    else:
+        return "X"
+
+
 def floor_minute(time, cadence=12):
     return time - datetime.timedelta(minutes=time.minute % cadence)
 
 
+C_CLASS_FLARES = [
+    1
+]
+
+B_CLASS_FLARES = [
+    2
+]
+
+M_CLASS_FLARES = [
+    3
+]
+
+X_CLASS_FLARES = [
+    4
+]
+
 def main():
     # Choose which flares to plot.
     # ABC Flares
-    info_df = pd.read_csv("ABC_list.txt")
-    properties_df = pd.read_csv("Data_ABC.csv")
+    abc_info_df = pd.read_csv("ABC_list.txt")
+    abc_properties_df = pd.read_csv("Data_ABC.csv")
     # MX Flares
-    # info_df = pd.read_csv("MX_list.txt")
-    # properties_df = pd.read_csv("Data_MX.csv")
+    mx_info_df = pd.read_csv("MX_list.txt")
+    mx_properties_df = pd.read_csv("Data_MX.csv")
 
     # Define input for flare.
     flare_index = 9  # Valid: 0 to 765
@@ -71,50 +98,136 @@ def main():
 
     # Convert time strings to datetime objects for cleaned info data.
     for time_string in ["time_start", "time_peak", "time_end"]:
-        info_df[time_string] = \
-            info_df[time_string].apply(parse_tai_string)
+        abc_info_df[time_string] = \
+            abc_info_df[time_string].apply(parse_tai_string)
+        mx_info_df[time_string] = \
+            mx_info_df[time_string].apply(parse_tai_string)
 
     # Convert T_REC string to datetime objects.
-    properties_df["T_REC"] = \
-        properties_df["T_REC"].apply(parse_tai_string)
+    abc_properties_df["T_REC"] = \
+        abc_properties_df["T_REC"].apply(parse_tai_string)
+    mx_properties_df["T_REC"] = \
+        mx_properties_df["T_REC"].apply(parse_tai_string)
 
-    # Find NOAA AR number and timestamp from user input in info dataframe.
-    noaa_ar = info_df["nar"][flare_index]
-    timestamp = floor_minute(info_df["time_start"][flare_index])
+    # Label flares by B, C, M, and X.
+    abc_info_df["xray_class"] = \
+        abc_info_df["xray_class"].apply(classify_flare)
+    mx_info_df["xray_class"] = \
+        mx_info_df["xray_class"].apply(classify_flare)
 
-    # Find corresponding ending index in properties dataframe.
-    abc_end_series = properties_df.loc[
-        properties_df["T_REC"] == timestamp]
-    abc_end_index = abc_end_series.loc[
-        abc_end_series['NOAA_AR'] == noaa_ar].index.tolist()[0]
+    # Get first 40 of B and C class flares, round down their minutes.
+    b_df = abc_info_df.loc[abc_info_df["xray_class"] == "B"].head(58)
+    c_df = abc_info_df.loc[abc_info_df["xray_class"] == "C"].head(56)
+    bc_info = pd.concat([b_df, c_df])
+    bc_info["time_start"] = \
+        bc_info["time_start"].apply(floor_minute)
 
-    # Find corresponding starting index in properties dataframe, if it exists.
-    abc_start_index = abc_end_index
-    for i in range(time_range * 5 - 1):
-        if abc_end_index - i >= 0:
-            if properties_df["NOAA_AR"][abc_end_index - i] == noaa_ar:
-                abc_start_index = abc_end_index - i
+    # Find the respective timestamp in the ABC data file.
+    bc_data = pd.DataFrame()
+    for index, row in bc_info.iterrows():
+        timestamp = row["time_start"]
+        bc_series = abc_properties_df.loc[
+            abc_properties_df["T_REC"] == timestamp].head(1)
+        bc_series.insert(0, "CLASS", row["xray_class"])
+        bc_data = pd.concat([bc_data, bc_series])
 
-    # Plot specified flare properties over the specified time.
-    fig, ax = plt.subplots(6, 3, figsize=(18, 20))
-    row, col = 0, 0
-    for flare_property in FLARE_PROPERTIES:
-        property_df = properties_df[["T_REC", flare_property]]
-        print(property_df["T_REC"])
-        property_df.iloc[abc_start_index:abc_end_index].plot(
-            x="T_REC", y=flare_property, ax=ax[row, col], legend=False)
-        ax[row, col].set_ylabel(flare_property)
-        ax[row, col].set_title(
-            f"Total {flare_property} from {properties_df['T_REC'].values[0]}")
+    # Get first 40 of M and X class flares, round down their minutes.
+    m_df = mx_info_df.loc[mx_info_df["xray_class"] == "M"].head(45)
+    x_df = mx_info_df.loc[mx_info_df["xray_class"] == "X"].head(100)
+    mx_info = pd.concat([m_df, x_df])
+    mx_info["time_start"] = \
+        mx_info["time_start"].apply(floor_minute)
 
-        col += 1
-        if col == 3:
-            col = 0
-            row += 1
+    # Find the respective timestamp in the MX data file.
+    mx_data = pd.DataFrame()
+    for index, row in mx_info.iterrows():
+        timestamp = row["time_start"]
+        mx_series = mx_properties_df.loc[
+            mx_properties_df["T_REC"] == timestamp].head(1)
+        mx_series.insert(0, "CLASS", row["xray_class"])
+        mx_data = pd.concat([mx_data, mx_series])
 
+    # Combine BC and MX class flares into one dataframe and plot.
+    colors = ["cyan", "lime", "red", "magenta"]
+    columns = [
+        'ABSNJZH',
+        'AREA_ACR',
+        'MEANGAM',
+        'MEANGBH',
+        'MEANGBT',
+        'MEANGBZ',
+        'MEANJZD',
+        'MEANJZH',
+        'MEANPOT',
+        'MEANSHR',
+        'R_VALUE',
+        'SAVNCPP',
+        'SHRGT45',
+        'TOTPOT',
+        'TOTUSJH',
+        'TOTUSJZ',
+        'USFLUX',
+    ]
+    flares_df = pd.concat([bc_data, mx_data])
+    flares_df[columns] = MinMaxScaler().fit_transform(flares_df[columns])
+    plt.style.use('dark_background')
+    fig = plt.figure(figsize=(25, 12))
+    pd.plotting.parallel_coordinates(flares_df.drop("T_REC", axis=1).
+                                     drop("NOAA_AR", axis=1),
+                                     "CLASS", color=colors)
     fig.tight_layout()
+    fig.show()
+    fig.savefig("all_parameters")
+
+    # COMMENT OUT BELOW CODE FOR SINGLE PLOTS
+
+
+
+    # # Find corresponding ending index in properties dataframe.
+    # abc_end_series = abc_properties_df.loc[
+    #     abc_properties_df["T_REC"] == abc_timestamp]
+    # abc_end_index = abc_end_series.loc[
+    #     abc_end_series['NOAA_AR'] == abc_noaa].index.tolist()[0]
+    # mx_end_series = mx_properties_df.loc[
+    #     mx_properties_df["T_REC"] == mx_timestamp]
+    # mx_end_index = mx_end_series.loc[
+    #     mx_end_series['NOAA_AR'] == mx_noaa].index.tolist()[0]
+    #
+    # # Find corresponding starting index in properties dataframe, if it exists.
+    # abc_start_index = abc_end_index
+    # for i in range(time_range * 5 - 1):
+    #     if abc_end_index - i >= 0:
+    #         if abc_properties_df["NOAA_AR"][abc_end_index - i] == abc_noaa:
+    #             abc_start_index = abc_end_index - i
+    # mx_start_index = mx_end_index
+    # for i in range(time_range * 5 - 1):
+    #     if mx_end_index - i >= 0:
+    #         if mx_properties_df["NOAA_AR"][mx_end_index - i] == abc_noaa:
+    #             mx_start_index = mx_end_index - i
+    #
+    # # Plot specified flare properties over the specified time.
+    # fig, ax = plt.subplots(6, 3, figsize=(18, 20))
+    # row, col = 0, 0
+    # pd.plotting.parallel_coordinates(abc_properties_df.iloc[abc_start_index], "T_REC")
+    # plt.tight_layout()
+    # plt.show()
+    # for flare_property in FLARE_PROPERTIES:
+    #     property_df = properties_df[["T_REC", flare_property]]
+    #     print(property_df["T_REC"])
+    #     property_df.iloc[abc_start_index:abc_end_index].plot(
+    #         x="T_REC", y=flare_property, ax=ax[row, col], legend=False)
+    #     ax[row, col].set_ylabel(flare_property)
+    #     ax[row, col].set_title(
+    #         f"Total {flare_property} from {properties_df['T_REC'].values[0]}")
+    #
+    #     col += 1
+    #     if col == 3:
+    #         col = 0
+    #         row += 1
+    #
+    # fig.tight_layout()
     # fig.show()
-    fig.savefig("abc_output")
+    # fig.savefig("abc_output")
 
 
     # print(abc_properties_df)
