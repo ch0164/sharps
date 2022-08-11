da9b6c0fde636a6adfe5374d77b88c678fa963c1
Christian Hall <ch0164@uah.edu>
2022-08-11 11:27:40 -0500

    So much data...

--- mean_classifier.py
+++ mean_classifier.py
@@ -8,6 +8,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import os
+from csv import writer
 
 from sklearn.metrics import classification_report
 
@@ -30,23 +31,22 @@ FLARE_PROPERTIES = [
     'TOTUSJZ',
     'USFLUX',
 ]
+FLARE_PROPERTIES += ['d_l_f', 'g_s', 'slf']
 
 FLARE_LABELS = ["B", "C", "M", "X"]
 COINCIDENCES = ["all", "coincident", "noncoincident"]
-# COINCIDENCES = ["all"]
 
-to_drop = [
-    'MEANGBZ',
-    'MEANGBH',
-    'TOTUSJZ',
-    'TOTUSJH',
-    'SAVNCPP',
-    'MEANPOT',
-    'TOTPOT',
-    'MEANSHR',
-    'SHRGT45',
-    'AREA_ACR'
+TIME_COMBINATIONS = [
+    '0h-12h',
+    '6h-18h',
+    '12h-24h',
+    '0h-6h',
+    '6h-12h',
+    '12h-18h',
+    '6h-24h',
+    '0h-24h'
 ]
+# COINCIDENCES = ["all"]
 
 
 def parse_tai_string(tstr, datetime=True):
@@ -93,11 +93,38 @@ def classify(mean_df, std_df, param, x):
     new_mean2 = c_mean + c_std
     new_mean = (new_mean1 + new_mean2) / 2
 
-    if x < new_mean:  #  + (3 * m_std)
+    if x < new_mean:  # + (3 * m_std)
         return "ABC"
     else:
         return "MX"
 
+
+def ravel_conf_matrix(conf_matrix):
+    return conf_matrix[0][0], conf_matrix[0][1], \
+           conf_matrix[1][0], conf_matrix[1][1]
+
+
+def tss(conf_matrix):
+    tp, fp, fn, tn = ravel_conf_matrix(conf_matrix)  # a, b, c, d
+    sensitivity = tp / (tp + fn)
+    specificity = fp / (fp + tn)
+    return sensitivity - specificity  # H - F
+
+
+def orss(conf_matrix):
+    tp, fp, fn, tn = ravel_conf_matrix(conf_matrix)  # a, b, c, d
+    return (tp * tn - fp * fn) - (tp * tn + fp * fn)
+
+
+def sedi(conf_matrix):
+    tp, fp, fn, tn = ravel_conf_matrix(conf_matrix)  # a, b, c, d
+    sens = tp / (tp + fn)
+    spec = fp / (fp + tn)
+    numerator = np.log(spec) - np.log(sens) - np.log(1 - spec) + np.log(
+        1 - sens)
+    denominator = np.log(spec) + np.log(sens) + np.log(1 - spec) + np.log(
+        1 - sens)
+    return numerator / denominator
 
 
 def main():
@@ -115,6 +142,7 @@ def main():
 
     # 2014 dataset
     root_directory = "classifiers/"
+    stats_directory = f"statistics/"
     data_directory = f"{root_directory}data_2014/"
 
     info_df = pd.read_csv(f"{root_directory}2013_2014_flare_info.csv")
@@ -122,91 +150,176 @@ def main():
         info_df[time_string] = \
             info_df[time_string].apply(parse_tai_string)
 
-    mean_df = pd.read_csv(f"{root_directory}mean_{'all'}.csv")
-    std_df = pd.read_csv(f"{root_directory}std_{'all'}.csv")
-    for is_coincident in COINCIDENCES:
-        temp_df = info_df
-        new_df = pd.DataFrame()
-        if is_coincident == "coincident":
-            info_df = info_df.loc[info_df["is_coincident"] == True]
-        elif is_coincident == "noncoincident":
-            info_df = info_df.loc[info_df["is_coincident"] == False]
-        accuracies = pd.DataFrame(columns=FLARE_PROPERTIES)
-        for flare_property in FLARE_PROPERTIES:
-            y_true, y_pred = [], []
-            flare_conf = np.zeros((2, 2), dtype=int)
-            for flare_index, row in info_df.iterrows():
-                df_needed = pd.DataFrame(columns=FLARE_PROPERTIES)
-                print(flare_index, "/", info_df.shape[0])
-                # Find NOAA AR number and timestamp from user input in info dataframe.
-                noaa_ar = row["nar"]
-                timestamp = floor_minute(row["time_start"])
-                flare_class = row["xray_class"]
+    for time_combo in TIME_COMBINATIONS:
+        mean_df = pd.read_csv(f"{stats_directory}{'all'}/{time_combo}/{time_combo}_mean_{'all'}.csv")
+        std_df = pd.read_csv(f"{stats_directory}{'all'}/{time_combo}/{time_combo}_std_{'all'}.csv")
+        for is_coincident in COINCIDENCES:
+            temp_df = info_df
+            new_df = pd.DataFrame()
+            if is_coincident == "coincident":
+                info_df = info_df.loc[info_df["is_coincident"] == True]
+            elif is_coincident == "noncoincident":
+                info_df = info_df.loc[info_df["is_coincident"] == False]
+            accuracies = pd.DataFrame(columns=FLARE_PROPERTIES)
+            for flare_property in FLARE_PROPERTIES:
+                y_true, y_pred = [], []
+                flare_conf = np.zeros((2, 2), dtype=int)
+                for flare_index, row in info_df.iterrows():
+                    df_needed = pd.DataFrame(columns=FLARE_PROPERTIES)
+                    print(flare_index, "/", info_df.shape[0])
+                    # Find NOAA AR number and timestamp from user input in info dataframe.
+                    noaa_ar = row["nar"]
+                    timestamp = floor_minute(row["time_start"])
+                    flare_class = row["xray_class"]
 
-                if flare_class in ["B", "C"]:
-                    properties_df = abc_properties_df
-                else:
-                    properties_df = mx_properties_df
+                    if flare_class in ["B", "C"]:
+                        properties_df = abc_properties_df
+                    else:
+                        properties_df = mx_properties_df
 
-                # Find corresponding ending index in properties dataframe.
-                end_series = properties_df.loc[properties_df["T_REC"] == timestamp]
-                if end_series.empty or (
-                        end_series.loc[end_series['NOAA_AR'] == noaa_ar]).empty:
-                    continue
-                else:
-                    end_index = \
-                        end_series.loc[end_series['NOAA_AR'] == noaa_ar].index.tolist()[0]
+                    # Find corresponding ending index in properties dataframe.
+                    end_series = properties_df.loc[
+                        properties_df["T_REC"] == timestamp]
+                    if end_series.empty or (
+                            end_series.loc[end_series['NOAA_AR'] == noaa_ar]).empty:
+                        continue
+                    else:
+                        end_index = \
+                            end_series.loc[
+                                end_series['NOAA_AR'] == noaa_ar].index.tolist()[0]
 
-                # Find corresponding starting index in properties dataframe, if it exists.
-                start_index = end_index
-                for i in range(time_range * 5 - 1):
-                    if end_index - i >= 0:
-                        if properties_df["NOAA_AR"][end_index - i] == noaa_ar:
-                            start_index = end_index - i
+                    # Find corresponding starting index in properties dataframe, if it exists.
+                    start_index = end_index
+                    for i in range(time_range * 5 - 1):
+                        if end_index - i >= 0:
+                            if properties_df["NOAA_AR"][end_index - i] == noaa_ar:
+                                start_index = end_index - i
 
-                # Make sub-dataframe of this flare
-                local_properties_df = properties_df.iloc[start_index:end_index + 1]
-                # local_properties_df.loc[:, 'xray_class'] = flare_class
-                # local_properties_df.loc[:, 'time_start'] = start_time
-                # local_properties_df.loc[:, 'is_coincident'] = coincidence
-                # local_properties_df.loc[:, 'flare_index'] = flare_index
-                df_needed = local_properties_df
+                    # Make sub-dataframe of this flare
+                    local_properties_df = properties_df.iloc[
+                                          start_index:end_index + 1]
+                    # local_properties_df.loc[:, 'xray_class'] = flare_class
+                    # local_properties_df.loc[:, 'time_start'] = start_time
+                    # local_properties_df.loc[:, 'is_coincident'] = coincidence
+                    # local_properties_df.loc[:, 'flare_index'] = flare_index
+                    df_needed = local_properties_df
 
-                mean = df_needed[flare_property].mean()
-                pred_class = classify(mean_df, std_df, flare_property, mean)
-                if flare_class in "ABC":
-                    y = "ABC"
-                else:
-                    y = "MX"
-                y_true.append(y)
-                y_pred.append(pred_class)
-                flare_class_num, pred_class_num = flare_to_num(flare_class), flare_to_num(pred_class)
-                if pred_class == flare_class:
-                    flare_conf[flare_class_num][flare_class_num] += 1
-                else:
-                    flare_conf[flare_class_num][pred_class_num] += 1
-            print(flare_conf)
-            sns.heatmap(flare_conf, annot=True, cmap="Blues", cbar=False,
-                        fmt="d",square=True,
-                        xticklabels=["ABC", "MX"], yticklabels=["ABC", "MX"])
-            plt.title(f"Mean-based Prediction on {flare_property} for "
-                      f"{is_coincident.capitalize()} Flares")
-            plt.tight_layout()
-            plt.savefig(f"{root_directory}{is_coincident}/{flare_property}_{is_coincident}_mean.jpeg")
-            plt.show()
+                    mean = df_needed[flare_property].mean()
+                    pred_class = classify(mean_df, std_df, flare_property, mean)
+                    if flare_class in "ABC":
+                        y = "ABC"
+                    else:
+                        y = "MX"
+                    y_true.append(y)
+                    y_pred.append(pred_class)
+                    flare_class_num, pred_class_num = flare_to_num(
+                        flare_class), flare_to_num(pred_class)
+                    if pred_class == flare_class:
+                        flare_conf[flare_class_num][flare_class_num] += 1
+                    else:
+                        flare_conf[flare_class_num][pred_class_num] += 1
+                print(flare_conf)
+                sns.heatmap(flare_conf, annot=True, cmap="Blues", cbar=False,
+                            fmt="d", square=True,
+                            xticklabels=["ABC", "MX"], yticklabels=["ABC", "MX"])
+                plt.title(f"Mean-based Prediction on {flare_property} for "
+                          f"{is_coincident.capitalize()} Flares")
+                plt.tight_layout()
+                plt.savefig(
+                    f"{root_directory}{is_coincident}/{time_combo}/{time_combo}_{flare_property}_{is_coincident}_mean.jpeg")
+                plt.show()
 
-            correct = flare_conf[0][0] + flare_conf[1][1]
-            incorrect = flare_conf[0][1] + flare_conf[1][0]
-            total = correct + incorrect
-            accuracies[flare_property] = [correct / total]
+                cr = classification_report(y_true, y_pred,
+                                           target_names=["ABC", "MX"],
+                                           output_dict=True)
+                df = pandas.DataFrame(cr).T
+                filename = f"{root_directory}{is_coincident}/{time_combo}/classification_reports/{time_combo}_{flare_property}_classification_report_{is_coincident}.csv"
+                df.to_csv(filename)
+                with open(filename, "a", newline="") as f:
+                    writer_obj = writer(f)
+                    writer_obj.writerow([])
+                    writer_obj.writerow(["TSS", tss(flare_conf)])
+                    writer_obj.writerow(["ORSS", orss(flare_conf)])
+                    writer_obj.writerow(["SEDI", sedi(flare_conf)])
 
-            cr = classification_report(y_true, y_pred, target_names=["ABC", "MX"], output_dict=True)
-            df = pandas.DataFrame(cr).transpose()
-            df.to_csv(f"{root_directory}{is_coincident}/classification_reports/{flare_property}_classification_report_{is_coincident}.csv")
+            info_df = temp_df
 
-        accuracies.to_csv(f"{root_directory}{is_coincident}/accuracies_{is_coincident}.csv")
-        info_df = temp_df
+
+def generate_report_summary():
+    for time_combo in TIME_COMBINATIONS:
+        mx_recall = {
+            "all": [],
+            "coincident": [],
+            "noncoincident": []
+        }
+        tss = {
+            "all": [],
+            "coincident": [],
+            "noncoincident": []
+        }
+        orss = {
+            "all": [],
+            "coincident": [],
+            "noncoincident": []
+        }
+        sedi = {
+            "all": [],
+            "coincident": [],
+            "noncoincident": []
+        }
+        for coincidence in COINCIDENCES:
+            for flare_property in FLARE_PROPERTIES:
+                file = f"{'classifiers/'}{coincidence}/{time_combo}/classification_reports/{time_combo}_{flare_property}_classification_report_{coincidence}.csv"
+                with open(file, "r") as f:
+                    for line in f.readlines():
+                        if "MX" in line:
+                            mx_recall[coincidence].append(line.split(",")[2])
+                        elif "TSS" in line:
+                            tss[coincidence].append(line.split(",")[1].strip())
+                        elif "ORSS" in line:
+                            orss[coincidence].append(line.split(",")[1].strip())
+                        elif "SEDI" in line:
+                            sedi[coincidence].append(line.split(",")[1].strip())
+        with open(f"classifiers/{time_combo}_mean_classification_summary.csv", "w", newline="") as f:
+            writer_obj = writer(f)
+            writer_obj.writerow([""] + FLARE_PROPERTIES)
+            for coincidence in COINCIDENCES:
+                writer_obj.writerow(
+                    [f"MX Recall ({coincidence.capitalize()})"] + mx_recall[
+                        coincidence])
+            writer_obj.writerow(
+                ["MX Recall Difference (Coincident - Noncoincident)"] + \
+                [str(float(coin) - float(noncoin)) for coin, noncoin in
+                 zip(mx_recall["coincident"], mx_recall["noncoincident"])])
+            writer_obj.writerow([""])
+            for coincidence in COINCIDENCES:
+                writer_obj.writerow(
+                    [f"True Skill Score ({coincidence.capitalize()})"] + tss[
+                        coincidence])
+            writer_obj.writerow(
+                ["TSS Difference (Coincident - Noncoincident)"] + \
+                [str(float(coin) - float(noncoin)) for coin, noncoin in
+                 zip(tss["coincident"], tss["noncoincident"])])
+            writer_obj.writerow([""])
+            for coincidence in COINCIDENCES:
+                writer_obj.writerow(
+                    [f"Odds Ratio Skill Score ({coincidence.capitalize()})"] + orss[
+                        coincidence])
+            writer_obj.writerow(
+                ["ORSS Difference (Coincident - Noncoincident)"] + \
+                [str(float(coin) - float(noncoin)) for coin, noncoin in
+                 zip(orss["coincident"], orss["noncoincident"])])
+            writer_obj.writerow([""])
+            for coincidence in COINCIDENCES:
+                writer_obj.writerow([
+                                        f"Symmetric Extremal Dependence Index ({coincidence.capitalize()})"] +
+                                    sedi[coincidence])
+            writer_obj.writerow(
+                ["SEDI Difference (Coincident - Noncoincident)"] + \
+                [str(float(coin) - float(noncoin)) for coin, noncoin in
+                 zip(sedi["coincident"], sedi["noncoincident"])])
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    generate_report_summary()
--- statistics.py
+++ statistics.py
@@ -26,8 +26,9 @@ FLARE_PROPERTIES = [
     'TOTUSJZ',
     'USFLUX',
 ]
+FLARE_PROPERTIES += ['d_l_f', 'g_s', 'slf']
 
-to_drop = ['d_l_f', 'g_s', 'slf']
+# to_drop = ['d_l_f', 'g_s', 'slf']
 
 mean_df = pd.DataFrame(columns=FLARE_PROPERTIES)
 median_df = pd.DataFrame(columns=FLARE_PROPERTIES)
@@ -41,68 +42,80 @@ statistics = {
     "mean": mean_df,
     "median": median_df,
     "std": std_df,
-    "delta24h": delta24_df,
-    "delta12h": delta12_df,
-    "delta6h": delta6_df,
+    # "delta24h": delta24_df,
+    # "delta12h": delta12_df,
+    # "delta6h": delta6_df,
     "entropy": entropy_df,
+}
+
+time_combinations = {
+    "0h-12h": range(1, 60),
+    "6h-18h": range(30, 90),
+    "12h-24h": range(60, 120),
+    "0h-6h": range(1, 30),
+    "6h-12h": range(30, 60),
+    "12h-18h": range(60, 90),
+    "6h-24h": range(90, 120),
+    "0h-24h": range(1, 120)
 }
 
 
 def generate_idealized_statistics(is_coincident):
-    for stat_label in statistics.keys():
-        for flare_label in flare_labels:
-            flare_df = pd.read_csv(f"{idealized_flares}/{is_coincident}/{flare_label}_idealized_flare.csv")
-            flare_df.drop(to_drop, inplace=True, axis=1)
-            flare_df.drop("Unnamed: 0", inplace=True, axis=1)
-            flare_df.drop([0], inplace=True, axis=0)
+    for time_combo, time_range in time_combinations.items():
+        for stat_label in statistics.keys():
+            for flare_label in flare_labels:
+                flare_df = pd.read_csv(f"{idealized_flares}/{is_coincident}/{flare_label}_idealized_flare_{is_coincident}.csv")
+                flare_df.drop("Unnamed: 0", inplace=True, axis=1)
+                flare_df = flare_df.iloc[time_range]
 
-            df = pd.DataFrame(columns=FLARE_PROPERTIES)
-            for flare_property in FLARE_PROPERTIES:
-                if stat_label in "mean":
-                    df[flare_property] = [flare_df[flare_property].mean()]
-                elif stat_label in "median":
-                    df[flare_property] = [flare_df[flare_property].median()]
-                elif stat_label in "std":
-                    df[flare_property] = [flare_df[flare_property].std()]
-                elif stat_label in "delta24h":
-                    t0 = flare_df[flare_property][1]
-                    t1 = flare_df[flare_property][flare_df.shape[0]]
-                    delta = t1 - t0
-                    df[flare_property] = [delta]
-                elif stat_label in "delta12h":
-                    t0 = flare_df[flare_property][1]
-                    t1 = flare_df[flare_property][flare_df.shape[0] // 2]
-                    t2 = flare_df[flare_property][flare_df.shape[0]]
-                    delta1 = t1 - t0
-                    delta2 = t2 - t1
-                    df[flare_property] = [delta1, delta2]
-                elif stat_label in "delta6h":
-                    t0 = flare_df[flare_property][1]
-                    t1 = flare_df[flare_property][flare_df.shape[0] // 4]
-                    t2 = flare_df[flare_property][flare_df.shape[0] // 2]
-                    t3 = flare_df[flare_property][(flare_df.shape[0] // 2) + (flare_df.shape[0] // 4)]
-                    t4 = flare_df[flare_property][flare_df.shape[0]]
-                    delta1 = t1 - t0
-                    delta2 = t2 - t1
-                    delta3 = t3 - t2
-                    delta4 = t4 - t3
-                    df[flare_property] = [delta1, delta2, delta3, delta4]
-                elif stat_label in "entropy":
-                    df[flare_property] = [differential_entropy(flare_df[flare_property])]
-            stat_df = statistics[stat_label]
-            statistics[stat_label] = pd.concat([stat_df, df])
+                df = pd.DataFrame(columns=FLARE_PROPERTIES)
 
-    for stat_label, stat_df in statistics.items():
-        if stat_label in "delta12h":
-            new_labels = [f"{label}{number}" for label in flare_labels for number in [1, 2]]
-        elif stat_label in "delta6h":
-            new_labels = [f"{label}{number}" for label in flare_labels for number in [1, 2, 3, 4]]
-        else:
-            new_labels = flare_labels
-        stat_df.index = new_labels
-        print(stat_label, stat_df)
-        stat_df.to_csv(f"statistics/{is_coincident}/{stat_label}_{is_coincident}.csv")
-        statistics[stat_label] = pd.DataFrame(columns=FLARE_PROPERTIES)
+                for flare_property in FLARE_PROPERTIES:
+                    if stat_label in "mean":
+                        df[flare_property] = [flare_df[flare_property].mean()]
+                    elif stat_label in "median":
+                        df[flare_property] = [flare_df[flare_property].median()]
+                    elif stat_label in "std":
+                        df[flare_property] = [flare_df[flare_property].std()]
+                    # elif stat_label in "delta24h":
+                    #     t0 = flare_df[flare_property][1]
+                    #     t1 = flare_df[flare_property][flare_df.shape[0]]
+                    #     delta = t1 - t0
+                    #     df[flare_property] = [delta]
+                    # elif stat_label in "delta12h":
+                    #     t0 = flare_df[flare_property][1]
+                    #     t1 = flare_df[flare_property][flare_df.shape[0] // 2]
+                    #     t2 = flare_df[flare_property][flare_df.shape[0]]
+                    #     delta1 = t1 - t0
+                    #     delta2 = t2 - t1
+                    #     df[flare_property] = [delta1, delta2]
+                    # elif stat_label in "delta6h":
+                    #     t0 = flare_df[flare_property][1]
+                    #     t1 = flare_df[flare_property][flare_df.shape[0] // 4]
+                    #     t2 = flare_df[flare_property][flare_df.shape[0] // 2]
+                    #     t3 = flare_df[flare_property][(flare_df.shape[0] // 2) + (flare_df.shape[0] // 4)]
+                    #     t4 = flare_df[flare_property][flare_df.shape[0]]
+                    #     delta1 = t1 - t0
+                    #     delta2 = t2 - t1
+                    #     delta3 = t3 - t2
+                    #     delta4 = t4 - t3
+                    #     df[flare_property] = [delta1, delta2, delta3, delta4]
+                    elif stat_label in "entropy":
+                        df[flare_property] = [differential_entropy(flare_df[flare_property])]
+                stat_df = statistics[stat_label]
+                statistics[stat_label] = pd.concat([stat_df, df])
+
+        for stat_label, stat_df in statistics.items():
+            # if stat_label in "delta12h":
+            #     new_labels = [f"{label}{number}" for label in flare_labels for number in [1, 2]]
+            # elif stat_label in "delta6h":
+            #     new_labels = [f"{label}{number}" for label in flare_labels for number in [1, 2, 3, 4]]
+            # else:
+            #     new_labels = flare_labels
+            stat_df.index = flare_labels
+            print(stat_label, stat_df)
+            stat_df.to_csv(f"statistics/{is_coincident}/{time_combo}/{time_combo}_{stat_label}_{is_coincident}.csv")
+            statistics[stat_label] = pd.DataFrame(columns=FLARE_PROPERTIES)
 
 
 def main():
